c2b5da6fc833614eb8fbd5c3bee921112b346cda
Amrish <amrishkpba@gmail.com>
2025-11-17 02:58:39 +0530

    first commit

---
+++ server/routes/userRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { checkAuth, login, signup, updateProfile } from "../controllers/userController.js";
+import { proctectRoute } from "../middleware/auth.js";
+
+
+
+
+const userRouter = express.Router();
+
+
+userRouter.post("/signup",signup)
+userRouter.post("/login",login)
+userRouter.put("/update-profile",proctectRoute,updateProfile)
+userRouter.get("/check",proctectRoute,checkAuth)
+
+
+export default userRouter;
