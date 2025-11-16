f6d34dfb8811a74380bfda84879c29484419af2f
Amrish <amrishkpba@gmail.com>
2025-11-17 03:23:52 +0530

    server.js updated for vercel

--- server/server.js
+++ server/server.js
@@ -69,7 +69,12 @@ await connectDB();
 
 
 
-
+if(process.env.NODE_ENV !== "production"){
 const PORT = process.env.PORT || 5000;
 
 server.listen(PORT , ()=> console.log("server is running on PORT :"+PORT))
+}
+
+// export server for vercel
+
+export default server;
